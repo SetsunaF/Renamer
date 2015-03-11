6bc8cf84ff9acffc1323d3610fddac848cc8d78c
SetsunaF <setsunafseiei435@hotmail.com>
2015-03-10 21:22:30 -0500

    updated references directory

--- Renamer/Form1.Designer.cs
+++ Renamer/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(425, 208);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
