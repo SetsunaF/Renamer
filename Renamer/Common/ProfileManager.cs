14a9e4e92651f2f22eeeb969a0a68f96e5c2a199
SetsunaF <setsunafseiei435@hotmail.com>
2015-03-11 12:07:23 -0500

    added profile manager and regex helper classes

---
+++ Renamer/Common/ProfileManager.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+using Renamer.Models;
+
+namespace Renamer.Common
+{
+    public class ProfileManager
+    {
+        public List<Profile> ProfileList = new List<Profile>();
+        private string profileDirectory;
+        private ComboBox comboBox;
+
+        public ProfileManager(ComboBox combo)
+        {
+            string appDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            profileDirectory = appDirectory + "\\Profiles";
+
+            comboBox = combo;
+
+            CreateProfileFolder();
+            ReloadProfiles();
+        }
+
+        //stackoverflow.com/questions/600869/how-to-bind-a-list-to-a-combobox-winforms
+        public void PopulateProfiles()
+        {
+            comboBox.Items.Clear();
+            comboBox.Text = "";
+
+            foreach (var profile in ProfileList)
+                comboBox.Items.Add(profile.Name);
+
+            //comboBox.DataSource = null;
+            //comboBox.DataSource = ProfileList;
+            //comboBox.DisplayMember = "Name";
+        }
+
+        private void ReloadProfiles()
+        {
+            ProfileList.Clear();
+            string[] files = Directory.GetFiles(profileDirectory, "*.json");
+
+            foreach (var file in files)
+            {
+                ProfileList.Add(new Profile(file));
+            }
+
+            ProfileList = new List<Profile>(ProfileList.OrderBy(o => o.Name));
+        }
+
+        private void CreateProfileFolder()
+        {
+            if (!Directory.Exists(profileDirectory))
+                Directory.CreateDirectory(profileDirectory);
+        }
+
+        //Save profile of index...
+        private bool SaveProfile(int index, bool overwrite = true)
+        {
+            //int index = comboBox.SelectedIndex;
+            string message;
+
+            if (!ProfileList[index].Save(overwrite, out message))
+            {
+                MessageBox.Show(message, "Save Profile", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Update(List<Filter> filters)
+        {
+            int index = comboBox.SelectedIndex;
+
+            ProfileList[index].Filters.Clear();
+            ProfileList[index].Filters = new List<Filter>(filters);
+
+            SaveProfile(index, true);
+        }
+
+        public bool AddProfile(Profile profile)
+        {
+            ProfileList.Add(profile);
+            int index = ProfileList.Count - 1;
+
+            if (!SaveProfile(index, false))
+            {
+                ProfileList.RemoveAt(index);
+                return false;
+            }
+
+            PopulateProfiles();
+            comboBox.SelectedIndex = comboBox.Items.Count - 1;
+
+            return true;
+        }
+
+        public void DeleteSelected()
+        {
+            int index = comboBox.SelectedIndex;
+            string message;
+
+            if (!ProfileList[index].Delete(out message))
+                MessageBox.Show(message, "Delete Profile", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                ProfileList.RemoveAt(index);
+                PopulateProfiles();
+            }
+        }
+
+        public void AssignProfile(out List<Filter> filterList)
+        {
+            filterList=new List<Filter>(ProfileList[comboBox.SelectedIndex].Filters);
+        }
+
+
+
+    }
+}
---
+++ Renamer/Common/RegexHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+using Newtonsoft.Json;
+
+namespace Renamer.Common
+{
+    public static class RegexHelper
+    {
+        private static List<string[]> data;
+
+        private static bool Load()
+        {
+            string json;
+
+            try
+            {
+                json = File.ReadAllText(Application.StartupPath + "\\RegexFilters.json");
+            }
+            catch (FileNotFoundException e)
+            {
+                MessageBox.Show(e.Message, "Renamer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            try
+            {
+                data = JsonConvert.DeserializeObject<List<string[]>>(json);
+            }
+            catch (JsonException e)
+            {
+                MessageBox.Show(e.Message, "Renamer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        //public static void SetData(ContextMenuStrip menu, TextBox textBox, Action<object, EventArgs> onTextChange)
+        public static void SetData(ContextMenuStrip menu, TextBox textBox)
+        {
+            if (!Load()) return;
+
+            foreach (var pair in data)
+            {
+                if (pair.Length == 0) menu.Items.Add(new ToolStripSeparator());
+
+                else
+                {
+                    var item = new ToolStripMenuItem(pair[0]);
+                    var regex = pair[1];
+
+                    item.Click += (sender, args) =>
+                    {
+                        //textBox.Text += regex;
+                        //textBox.Focus();
+                        //textBox.SelectionStart = textBox.Text.Length - regex.Length;
+
+                        var selectionStart = textBox.SelectionStart;
+                        if (textBox.SelectionLength > 0) selectionStart += textBox.SelectionLength;
+
+                        textBox.Text = textBox.Text.Insert(selectionStart, regex);
+                        textBox.Focus();
+                        textBox.SelectionStart = selectionStart;
+                        textBox.SelectionLength = regex.Length;
+
+                        //onTextChange is triggered automatically
+                        //onTextChange(null, null);
+                    };
+
+                    menu.Items.Add(item);
+                }
+            }
+        }
+    }
+}
